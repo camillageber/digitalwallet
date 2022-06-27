502fc6ca994a76d33bdd6856d28ed3665913c418
Camilla Geber <camilla.geber@gmail.com>
2022-06-27 13:22:34 -0300

    Iniciando o requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+        />
+      </form>
+    );
   }
 }
 
