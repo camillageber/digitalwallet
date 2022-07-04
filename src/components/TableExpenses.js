159273b16588a566ee635fb6b5c53b5a05e68b86
Camilla Geber <camilla.geber@gmail.com>
2022-07-04 12:25:13 -0300

    Adiciona o requisito 8

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import infoTable from './infoTable';
 
 class TableExpenses extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
@@ -14,6 +16,26 @@ class TableExpenses extends React.Component {
               ))}
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((expenseEl, index) => (
+              <tr key={ `${expenseEl.value}-${index}` }>
+                <td>{expenseEl.description}</td>
+                <td>{expenseEl.tag}</td>
+                <td>{expenseEl.method}</td>
+                <td>{parseFloat(expenseEl.value).toFixed(2)}</td>
+                <td>{expenseEl.exchangeRates[expenseEl.currency].name}</td>
+                <td>
+                  {parseFloat(expenseEl.exchangeRates[expenseEl.currency].ask)
+                    .toFixed(2)}
+                </td>
+                <td>
+                  {(parseFloat(expenseEl.value) * parseFloat(expenseEl
+                    .exchangeRates[expenseEl.currency].ask)).toFixed(2)}
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
@@ -22,5 +44,14 @@ class TableExpenses extends React.Component {
 
 // https://www.w3schools.com/tags/tag_thead.asp
 // https://www.w3schools.com/tags/tag_tr.asp
+// https://www.w3schools.com/tags/tag_tbody.asp
 
-export default connect()(TableExpenses);
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+TableExpenses.propTypes = {
+  expenses: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps)(TableExpenses);
